016af365c537d111a2f699739a73cd28b021ccb9
MohanDeredla <mohanreddyderedla148@gmail.com>
2025-07-11 13:03:11 +0530

    EmailLog controller implementation

--- EmailServiceApplication/src/main/java/com/example/EmailServiceApplication/Controller/EmailLogController.java
+++ EmailServiceApplication/src/main/java/com/example/EmailServiceApplication/Controller/EmailLogController.java
@@ -1,5 +1,22 @@
 package com.example.EmailServiceApplication.Controller;
 
-public class EmailLogController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.EmailServiceApplication.Models.EmailLog;
+import com.example.EmailServiceApplication.Repository.EmailLogRepository;
+
+@RestController
+@RequestMapping("/api/logs")
+public class EmailLogController {
+	@Autowired
+    private EmailLogRepository emailLogRepository;
+	@GetMapping
+	public List<EmailLog> getAllLogs(){
+		return emailLogRepository.findAll();
+	}
 }
