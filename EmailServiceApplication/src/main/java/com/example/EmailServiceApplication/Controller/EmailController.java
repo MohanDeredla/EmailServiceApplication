8add8f685f4ec318683bd9bae22510eadfa716c1
MohanDeredla <mohanreddyderedla148@gmail.com>
2025-07-11 12:59:49 +0530

    Added Api keys for EmailController and EmailLogController has created

--- EmailServiceApplication/src/main/java/com/example/EmailServiceApplication/Controller/EmailController.java
+++ EmailServiceApplication/src/main/java/com/example/EmailServiceApplication/Controller/EmailController.java
@@ -3,6 +3,7 @@ package com.example.EmailServiceApplication.Controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +12,11 @@ import com.example.EmailServiceApplication.Models.EmailRequest;
 import com.example.EmailServiceApplication.Service.EmailService;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/email")
 public class EmailController {
 	@Autowired
 	private EmailService emailService;
-	
+	@PostMapping("/send")
 	public Map<String,Object> sendEmail(@RequestBody EmailRequest request){
 		return emailService.sendEmail(request);
 		
---
+++ EmailServiceApplication/src/main/java/com/example/EmailServiceApplication/Controller/EmailLogController.java
@@ -0,0 +1,5 @@
+package com.example.EmailServiceApplication.Controller;
+
+public class EmailLogController {
+
+}
