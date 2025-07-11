f8d46c98062fc107184a63df0fbe9d6ea5f7d53d
MohanDeredla <mohanreddyderedla148@gmail.com>
2025-07-11 12:56:22 +0530

    EmailController has implemented and started working on business loic at Service layer in EmailService class

--- EmailServiceApplication/src/main/java/com/example/EmailServiceApplication/Controller/EmailController.java
+++ EmailServiceApplication/src/main/java/com/example/EmailServiceApplication/Controller/EmailController.java
@@ -1,10 +1,23 @@
 package com.example.EmailServiceApplication.Controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.EmailServiceApplication.Models.EmailRequest;
+import com.example.EmailServiceApplication.Service.EmailService;
 
 @RestController
 @RequestMapping
 public class EmailController {
-
+	@Autowired
+	private EmailService emailService;
+	
+	public Map<String,Object> sendEmail(@RequestBody EmailRequest request){
+		return emailService.sendEmail(request);
+		
+	}
 }
--- EmailServiceApplication/src/main/java/com/example/EmailServiceApplication/Service/EmailService.java
+++ EmailServiceApplication/src/main/java/com/example/EmailServiceApplication/Service/EmailService.java
@@ -1,8 +1,19 @@
 package com.example.EmailServiceApplication.Service;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
+
+import com.example.EmailServiceApplication.Models.EmailRequest;
 
 @Service
 public class EmailService {
+
+	public Map<String, Object> sendEmail(EmailRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 	
 }
